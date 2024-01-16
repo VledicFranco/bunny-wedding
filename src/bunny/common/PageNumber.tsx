5c8b145c717ab8bb9f22cfa045b487c6bf0c3f6f
Vledic | Franco <atfm05@gmail.com>
2024-01-16 00:27:10 -0600

    progress

--- src/bunny/common/PageNumber.tsx
+++ src/bunny/common/PageNumber.tsx
@@ -1,24 +1,23 @@
 import styled from "styled-components"
 import { CursiveFont } from "../CommonCss"
-import png from '../images/bg4.png'
+import png from '../images/Bouquet2.png'
 
 const Container = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: 20px;
     margin-bottom: 0px;
     font-size: 18px;
     ${CursiveFont}
     text-align: center;
     font-weight: bold;
-    height: 60px;
+    height: 120px;
     width: 100px;
     background-image: url(${png});
-    background-position: center;
+    background-position: bottom;
     background-repeat: no-repeat;
-    background-size: 90px;
+    background-size: 100px;
 `
 
 const PageNumber = ({ p }: { p: Number }) => 
